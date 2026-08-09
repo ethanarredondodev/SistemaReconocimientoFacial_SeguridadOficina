ad2bbe0d225f15b325c211ca888eedea1eab760b
EthanArredondo <ethansarredondo@gmail.com>
2026-08-09 01:25:27 -0500

    creacion automatica de admin

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .role import Role
+from .user import User
+from .office import Office
+from .role_office import RoleOffice
+from .user_office import UserOffice
+from .access_log import AccessLog
